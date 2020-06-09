6372caadebb058118d2c2edf42dadb15a2777a59
justhson <447227659@qq.com>
2020-06-09 17:23:35 +0800

    平安乐分享

--- 平安乐分享.js
+++ 平安乐分享.js
@@ -4,7 +4,7 @@
  * @Author: justhson
  * @Date: 2020-06-05 13:28:34
  * @LastEditors: justhson
- * @LastEditTime: 2020-06-09 14:57:12
+ * @LastEditTime: 2020-06-09 17:23:12
  */ 
 auto.waitFor();
 
@@ -27,10 +27,8 @@ setScreenMetrics(device.width, device.height);
 // log("进入乐分享今日推荐");
 // sleep(5000);
 
-let x = (864+1038)/2;
-let y = (525+600)/2;
-for(let i=1; i<=1; i++){
-    share(i, x, y);
+for(let i=0; i<10; i++){
+    share(i);
 }
 
 exit();
@@ -42,26 +40,42 @@ exit();
  * @param {type} 
  * @return: 
  */
-function share(i, x, y){
-    // click(x, y);
-    // toast("分享第"+i+"个帖子");
-    // log("分享第"+i+"个帖子");
-    // sleep(1000);
-    
-    // className("android.view.View").id("J-share-btn-friends").findOne().click();
-    // toast("微信分享");
-    // log("微信分享");
-    // sleep(1000);
+function share(i){
+    className("android.view.View").text("今日推荐").findOne().parent().child(3).child(i).child(0).child(0).child(1).child(1).child(0).click();
+    toast("分享第"+(i+1)+"个帖子");
+    log("分享第"+(i+1)+"个帖子");
+    sleep(2000);
 
-    // className("android.widget.TextView").text("微信").findOne().parent().click();
-    // toast("主微信");
-    // log("主微信");
-    // sleep(2000);
+    className("android.view.View").idContains("J-share-btn-friends").findOne().click();
+    toast("微信分享");
+    log("微信分享");
+    sleep(1000);
 
-    log(id('c').exists());
+    className("android.widget.TextView").text("微信").findOne().parent().click();
+    toast("主微信");
+    log("主微信");
+    sleep(2000);
 
-    // 间距为207
-    // swipe(x, y, x, y+207, 1000);   
-    // toast("上滑");
-    // log("上滑");
+    let bounds = className("android.widget.TextView").text("周潇").findOne().bounds();
+    click(bounds.centerX(), bounds.centerY());
+    toast("选择分享人");
+    log("选择分享人");
+    sleep(1000);
+
+    className("android.widget.Button").text("分享").findOne().click();
+    toast("分享");
+    log("分享");
+    sleep(1000);
+
+    className("android.widget.Button").text("返回平安口袋银行").findOne().click();
+    toast("返回平安");
+    log("返回平安");
+    sleep(1000);
+
+    if(text("266398c").exists()){
+        className("android.widget.Image").text("266398c").findOne().click();
+        toast("关闭弹窗");
+        log("关闭弹窗");
+        sleep(1000);
+    }
 }
