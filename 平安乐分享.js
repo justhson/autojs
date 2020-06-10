8fa9b9a7f4ae605f761873d0bfc2ba1c22f37548
justhson <447227659@qq.com>
2020-06-10 16:48:54 +0800

    优化

--- 平安乐分享.js
+++ 平安乐分享.js
@@ -4,40 +4,41 @@
  * @Author: justhson
  * @Date: 2020-06-05 13:28:34
  * @LastEditors: justhson
- * @LastEditTime: 2020-06-10 15:10:51
+ * @LastEditTime: 2020-06-10 16:48:01
  */ 
 auto.waitFor();
 
 setScreenMetrics(device.width, device.height);
 
 /* 通过APP名称打开 */
-// let appName = "融e购";
+// let appName = "微信";
 // launchApp(appName);
 
 /* 通过包名打开 */
 // let PackageName = getPackageName(appName);
 // log(PackageName);
-let packageName = "com.pingan.paces.ccms";
+let packageNameWX = "com.tencent.mm";
+let packageNamePAKDYH = "com.pingan.paces.ccms";
 
-// let bounds = className("android.view.View").idContains("z696483").findOne().bounds();
-// click(bounds.centerX(), bounds.centerY());
-// toast("进入乐分享");
-// log("进入乐分享");
-// sleep(1000);
+let bounds = className("android.view.View").idContains("z696483").findOne().bounds();
+click(bounds.centerX(), bounds.centerY());
+toast("进入乐分享");
+log("进入乐分享");
+sleep(1000);
 
-// bounds = className("android.view.View").text("积分任务").findOne().bounds();
-// click(bounds.centerX(), bounds.centerY());
-// toast("进入乐分享玩赚积分");
-// log("进入乐分享玩赚积分");
-// sleep(1000);
+bounds = className("android.view.View").text("积分任务").findOne().bounds();
+click(bounds.centerX(), bounds.centerY());
+toast("进入乐分享玩赚积分");
+log("进入乐分享玩赚积分");
+sleep(1000);
 
-// className("android.view.View").text("去完成").findOne().click();
-// toast("进入乐分享今日推荐");
-// log("进入乐分享今日推荐");
-// sleep(5000);
+className("android.view.View").text("去完成").findOne().click();
+toast("进入乐分享今日推荐");
+log("进入乐分享今日推荐");
+sleep(5000);
 
-for(let i=1; i<=10; i++){
-    share(i, packageName);
+for(let i=1; i<=1; i++){
+    share(i, packageNameWX, packageNamePAKDYH);
 }
 
 exit();
@@ -49,7 +50,7 @@ exit();
  * @param {type} 
  * @return: 
  */
-function share(i, packageName){
+function share(i, packageNameWX, packageNamePAKDYH){
     className("android.view.View").text("今日推荐").findOne().parent().child(3).child(i-1).child(0).child(0).child(1).child(1).child(0).click();
     toast("分享第"+i+"个帖子");
     log("分享第"+i+"个帖子");
@@ -76,27 +77,44 @@ function share(i, packageName){
     log("分享");
     sleep(1000);
 
-    className("android.widget.Button").text("留在微信").findOne().click();
-    toast("留在微信");
-    log("留在微信");
+    className("android.widget.Button").text("返回平安口袋银行").findOne().click();
+    toast("返回平安");
+    log("返回平安");
     sleep(1000);
+
+    launch(packageNameWX);
+    toast("打开微信");
+    log("打开微信");
+    sleep(5000);
+
+    className("android.widget.TextView").text("微信").findOne().parent().click();
+    toast("主微信");
+    log("主微信");
+    sleep(3000);
 
     className("android.view.View").text("自己").findOne().parent().parent().parent().parent().click();
     toast("进入聊天");
     log("进入聊天");
-    sleep(1000);
+    sleep(2000);
 
     for(let j=1; j<=3; j++){
         view(j);
     }
 
-    launch(packageName);
-    toast("打开");
-    log("打开");
+    launch(packageNamePAKDYH);
+    toast("打开平安口袋银行");
+    log("打开平安口袋银行");
     sleep(5000);
 
-    if(text("266398c").exists()){
+    if(className("android.widget.Image").text("266398c").exists()){
         className("android.widget.Image").text("266398c").findOne().click();
+        toast("关闭弹窗");
+        log("关闭弹窗");
+        sleep(1000);
+    }
+
+    if(className("android.view.View").idContains("J-share-btn-close").exists()){
+        className("android.view.View").idContains("J-share-btn-close").findOne().click();
         toast("关闭弹窗");
         log("关闭弹窗");
         sleep(1000);
